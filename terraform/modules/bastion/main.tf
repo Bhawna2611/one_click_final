8881a64ad2145b6511164b84d767ffd5060927b4
Bhawna Porwal <bhavna123porwal@gmail.com>
2026-02-25 20:08:15 +0530

    update

--- terraform/modules/bastion/main.tf
+++ terraform/modules/bastion/main.tf
@@ -29,7 +29,11 @@ resource "aws_instance" "bastion" {
   vpc_security_group_ids      = [aws_security_group.bastion_sg.id]
   associate_public_ip_address = true
 
-  
+  # Wait for the instance to pass EC2 status checks before Terraform marks it ready
+  timeouts {
+    create = "10m"
+  }
+
   tags = merge(var.common_tags, {
     Name = "bastion-host"
   })
@@ -45,6 +49,17 @@ resource "null_resource" "copy_key" {
     user        = "ubuntu"
     private_key = file(var.ssh_key_path)
     host        = aws_instance.bastion.public_ip
+
+    # Increase timeout so Terraform keeps retrying SSH until the instance is ready
+    timeout     = "5m"
+    agent       = false
+  }
+
+  # Wait for SSH to become available before copying the key
+  provisioner "remote-exec" {
+    inline = [
+      "echo 'SSH is ready - bastion host is up!'"
+    ]
   }
 
   provisioner "file" {
@@ -54,7 +69,8 @@ resource "null_resource" "copy_key" {
 
   provisioner "remote-exec" {
     inline = [
-      "chmod 400 /home/ubuntu/one__click.pem"
+      "chmod 400 /home/ubuntu/one__click.pem",
+      "echo 'Key copied and permissions set successfully'"
     ]
   }
 }
